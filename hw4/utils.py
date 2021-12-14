4fc785f941a7aa12e1a941d25f493042c7ad7890
Mikhail Kim <kimihailv@yandex.ru>
2021-12-15 02:07:02 +0300

    hw4 fixes

--- hw4/utils.py
+++ hw4/utils.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch
+import librosa
 from torchaudio import transforms
+from dataclasses import dataclass
 
 
 def init_weight(m):
@@ -23,20 +25,60 @@ def seed(seed):
     torch.backends.cudnn.benchmark = False
 
 
-class Featurizer(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.featurizer = transforms.MelSpectrogram(
-            sample_rate=22_050,
-            n_fft=1024,
-            win_length=1024,
-            hop_length=256,
-            n_mels=80,
-            center=False
-        )
-        self.pad_size = (1024 - 256) // 2
+@dataclass
+class MelSpectrogramConfig:
+    sr: int = 22050
+    win_length: int = 1024
+    hop_length: int = 256
+    n_fft: int = 1024
+    f_min: int = 0
+    f_max: int = 8000
+    n_mels: int = 80
+    power: float = 1.0
 
-    def forward(self, x):
+
+class MelSpectrogram(nn.Module):
+
+    def __init__(self, config: MelSpectrogramConfig):
+        super(MelSpectrogram, self).__init__()
+
+        self.config = config
+
+        self.mel_spectrogram = transforms.MelSpectrogram(
+            sample_rate=config.sr,
+            win_length=config.win_length,
+            hop_length=config.hop_length,
+            n_fft=config.n_fft,
+            f_min=config.f_min,
+            f_max=config.f_max,
+            n_mels=config.n_mels,
+            center=False,
+        )
+
+        # The is no way to set power in constructor in 0.5.0 version.
+        self.mel_spectrogram.spectrogram.power = config.power
+
+        # Default `torchaudio` mel basis uses HTK formula. In order to be compatible with WaveGlow
+        # we decided to use Slaney one instead (as well as `librosa` does by default).
+        mel_basis = librosa.filters.mel(
+            sr=config.sr,
+            n_fft=config.n_fft,
+            n_mels=config.n_mels,
+            fmin=config.f_min,
+            fmax=config.f_max
+        ).T
+        self.mel_spectrogram.mel_scale.fb.copy_(torch.tensor(mel_basis))
+        self.pad_size = (config.win_length - config.hop_length) // 2
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: Expected shape is [B, T]
+        :return: Shape is [B, n_mels, T']
+        """
         x = nn.functional.pad(x, (self.pad_size, self.pad_size), 'reflect')
-        spec = self.featurizer(x)
-        return spec.clamp(min=1e-5).log()
+        mel = self.mel_spectrogram(x) \
+            .clamp_(min=1e-5) \
+            .log_()
+
+        return mel
+
